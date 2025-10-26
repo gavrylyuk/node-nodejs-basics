8692b80bbbb49d8d9c6bbbc67624e5fccaae8084
Grygoriy Gavrylyuk <35098372+gavrylyuk@users.noreply.github.com>
2025-10-26 20:43:14 +0100

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-  // Write your code here
+  const rssVars = Object.entries(process.env)
+    .filter(([key]) => key.startsWith('RSS_'));
+
+  const output = rssVars.map(([key, value]) => `${key}=${value}`).join('; ');
+
+  console.log(output);
 };
 
 parseEnv();
