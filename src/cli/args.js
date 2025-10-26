16e27031073fb97fbb7716de6533a0fe5b982bff
Grygoriy Gavrylyuk <35098372+gavrylyuk@users.noreply.github.com>
2025-10-26 20:46:53 +0100

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = process.argv.slice(2);
+
+  const output = args.reduce((acc, curr, index, array) => {
+    if (curr.startsWith('--')) {
+      const key = curr.slice(2);
+      const value = array[index + 1];
+      acc.push(`${key} is ${value}`);
+    }
+    return acc;
+  }, []);
+
+  console.log(output.join(', '));
 };
 
 parseArgs();
