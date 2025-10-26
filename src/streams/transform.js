ae9fa13173a9e3a5ac73c7db31683b49e7692202
Grygoriy Gavrylyuk <35098372+gavrylyuk@users.noreply.github.com>
2025-10-26 21:30:15 +0100

    feat: implement transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-  // Write your code here
+  const transform = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversed = chunk.toString().split('').reverse().join('');
+      this.push(reversed);
+      callback();
+    }
+  });
+
+  console.log('Type something. Press Ctrl+Enter (on Windows) to finish.');
+
+  process.stdin.pipe(transform).pipe(process.stdout);
 };
 
 await transform();
